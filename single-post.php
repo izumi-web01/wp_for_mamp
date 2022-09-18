5f254b536c09123d679621f4acecff81457ee6b2
izumi <minyantokunyan@gmail.com>
2022-09-18 18:53:00 +0900

    foreach arrayの入れ子を復習しました

--- single-post.php
+++ single-post.php
@@ -40,94 +40,99 @@
     
     <?php endif; ?>
     <?php get_template_part('module01'); ?>
-<!-- //テスト -->
-    <?php 
-        $points = array(
-    '特徴1' => array(
-        'title01' => '楽しい',
-        'detail01' => '私とお話しするときっと楽しいです。',
-        'title02' => '親しみやすい',
-        'detail02' => 'わりと気取らない性格です。',
-        'title03' => '頑張り屋',
-        'detail03' => 'けっこう頑張る性格です',
-        'icon' =>'logo.png',
-        'mv' =>'kanda-gawa.jpg',
-      ),
-      '特徴2' => array(
-        'title01' => 'かわいい',
-        'detail01' => '顔がかわいい',
-        'title02' => 'せがちょうどいい',
-        'detail02' => 'ちょいどいいサイズ感です',
-        'title03' => '太ってない',
-        'detail03' => 'ダイエットしたからね。',
-        'icon' =>'logo.png',
-        'mv' =>'ethiopia.jpg',
-      ),
-      '特徴3' => array(
-        'title01' => '高学歴',
-        'detail01' => 'こう見えて早稲田卒です',
-        'title02' => '歌がうまい',
-        'detail02' => '実は昔歌手でした！',
-        'title03' => '子供がいない',
-        'detail03' => 'ひとりで身軽です',
-        'icon' =>'logo.png',
-        'mv' =>'lemon-paintings.jpg',
-      ), 
-    );
-    foreach( $points as $name => $arr ) :?>
 
+<!-- テスト -->
+<p>3つの特徴</p>
+<?php 
+$args = array(
+   '特徴1'=>array(
+        'big-title'=>'機能' ,
+        'big-img'=>'nicholai-do.jpg' ,
+        'title01'=>'機能の特徴1' ,
+        'detail01'=>'あああああああああああああ' ,
+        'img01'=>'logo-favicon.png',
+        'title02'=>'機能の特徴2' ,
+        'detail02'=>'いいいいいいいいいいいいい' ,
+        'img02'=>'logo-favicon.png',
+        'title03'=>'機能の特徴2' ,
+        'detail03'=>'ううううううううううううう' ,
+        'img03'=>'logo-favicon.png',),
+   '特徴2'=>array(
+        'big-title'=>'価格',
+        'big-img'=>'otoko-zaka.jpg' ,
+        'title01'=>'価格の特徴1' ,
+        'detail01'=>'おおおおおおおおおおおおおおおお' ,
+        'img01'=>'logo-favicon.png',
+        'title02'=>'価格の特徴2' ,
+        'detail02'=>'かかかかかかっかかかかっかかかｋ' ,
+        'img02'=>'logo-favicon.png',
+        'title03'=>'価格の特徴3' ,
+        'detail03'=>'' ,
+        'img03'=>'logo-favicon.png',),
+   '特徴3'=>array(
+    'big-title' =>'使いやすさ',
+        'big-img'=>'ethiopia.jpg' ,
+        'title01'=>'使いやすさの特徴1' ,
+        'detail01'=>'くくくくくくくくくくくくうっく' ,
+        'img01'=>'logo-favicon.png',
+        'title02'=>'使いやすさの特徴1' ,
+        'detail02'=>'けけけけけけけけけけけけけけｋ' ,
+        'img02'=>'logo-favicon.png',
+        'title03'=>'使いやすさの特徴1' ,
+        'detail03'=>'こここここここここここここお' ,
+        'img03'=>'logo-favicon.png',)
     
-        <div class="feature">
-            <figure>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php $mv = $arr['mv']; echo $mv; ?>" alt="Find! Ochanomizu">
+   );
+   foreach($args as $arg =>$arr):?>
 
+
+    <div class="feature">
+        <div class="feature__contents">
+            <p class="feature__contents__title">
+                <?php $bigTitle = $arr['big-title']; echo $bigTitle; ?>
+            </p>
+            <figure class>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php $bigImg = $arr['big-img']; echo $bigImg;?>" >
             </figure>
-            <div class="feature__contents">
-                <ul>
-                    <li>
-                        <div class="eyecatch">
-                            <div class="logo">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php $icon = $arr['icon']; echo $icon; ?>" alt="Find! Ochanomizu">
-                            </div>
-                            <p class="title"><?php $unit = $arr['title01']; echo $unit; ?></p>
-                        </div>
-                        <p class="detail"><?php $price = $arr['detail01']; echo $price; ?></p>
-                    </li>
-                
-                </ul>
+            <div class="eyecatch">
+                <div class="logo">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/<?php $img01 = $arr['img01']; echo $img01;?>" >
+
+                </div>
+                <p class="feature__title">
+                    <?php $title01 = $arr['title01'];echo $title01; ?>
+                </p>
             </div>
-            <div class="feature__contents">
-                <ul>
-                    <li>
-                        <div class="eyecatch">
-                            <div class="logo">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php $icon = $arr['icon']; echo $icon; ?>" alt="Find! Ochanomizu">
-                            </div>
-                            <p class="title"><?php $unit = $arr['title02']; echo $unit; ?></p>
-                        </div>
-                        <p class="detail"><?php $price = $arr['detail02']; echo $price; ?></p>
-                    </li>
-                
-                </ul>
+            <p class="feature__detail">
+                    <?php $detail01 = $arr['detail01'];echo $detail01; ?> 
+            </p>
+           
+            <div class="eyecatch">
+                <div class="logo">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/<?php $img02 = $arr['img02']; echo $img02;?>" >
+                </div>
+                <p class="feature__title">
+                    <?php $title02 = $arr['title02'];echo $title02; ?>
+                </p>
             </div>
-            <div class="feature__contents">
-                <ul>
-                    <li>
-                        <div class="eyecatch">
-                            <div class="logo">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php $icon = $arr['icon']; echo $icon; ?>" alt="Find! Ochanomizu">
-                            </div>
-                            <p class="title"><?php $unit = $arr['title03']; echo $unit; ?></p>
-                        </div>
-                        <p class="detail"><?php $price = $arr['detail03']; echo $price; ?></p>
-                    </li>
-                
-                </ul>
+            <p class="feature__detail">
+                    <?php $detail02 = $arr['detail02'];echo $detail02; ?> 
+            </p>
+           
+            <div class="eyecatch">
+            <div class="logo">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/<?php $img03 = $arr['img03']; echo $img03;?>" >
+
+                </div>
+                <p class="feature__title">
+                    <?php $title03 = $arr['title03'];echo $title03; ?>
+                </p>
             </div>
-            
+            <p class="feature__detail">
+                    <?php $detail03 = $arr['detail03'];echo $detail03; ?> 
+            </p>
         </div>
-    <?php endforeach; ?>
-   
-  </main>
+    </div>
+    <?php endforeach; ?></main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
